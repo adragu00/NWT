fa2f79b951233826b528ecdcdcf1688dd2856d3f
adragu00 <adragu00@fesb.hr>
2015-12-11 10:26:54 +0100

    commit

---
+++ NWT/Controllers/StudiesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using NWT.DAL;
+
+namespace NWT.Controllers
+{
+    public class StudiesController : Controller
+    {
+        private NWTContext db = new NWTContext();
+
+        // GET: Studies
+        public ActionResult Index()
+        {
+            return View(db.Studies.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NWT/Models/Study.cs
@@ -0,0 +1,19 @@
+﻿using NWT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NWT.Models
+{
+    public class Study
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+
+
+        // foreign keys
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
