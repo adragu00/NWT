dd576da39a817996dfe4b236dc0ee897b971e397
adragu00 <adragu00@fesb.hr>
2015-12-11 10:16:50 +0100

    Second commit

--- NWT/Controllers/HomeController.cs
+++ NWT/Controllers/HomeController.cs
@@ -19,12 +19,12 @@ namespace NWT.Controllers
 
             return View();
         }
-
+        /*
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
+        }*/
     }
 }
